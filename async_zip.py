fbe239565eaf9a66dd0b3b5f374f6d3b14f9d6ea
tantora <hoojoo.nbk.apid@gmail.com>
2019-09-14 14:23:15 +0900

    同時にwriteするファイル数に上限があったのでセマフォを追加する

--- async_zip.py
+++ async_zip.py
@@ -10,8 +10,8 @@ import aiofiles
 DIR = 'data'
 os.makedirs(DIR, exist_ok=True)
 
-ITEM_NUM = 5000
-DATA_SIZE = 1000
+ITEM_NUM = 100
+DATA_SIZE = 10000
 
 
 def randomname(n):
@@ -32,12 +32,14 @@ async def async_zip(filename, d):
 
 
 async def async_write(filename, z):
-    print("{} write start".format(filename))
-    # with open(filename, 'wb') as f:
-    async with aiofiles.open(filename, 'wb') as f:
-        f.write(z)
-    print("{} write finish".format(filename))
-    return filename
+    # セマフォで同時のwrite数を制限制限しておく
+    with await sem:
+        print("{} write start".format(filename))
+        # with open(filename, 'wb') as f:
+        async with aiofiles.open(filename, 'wb') as f:
+            f.write(z)
+        print("{} write finish".format(filename))
+        return filename
 
 
 async def async_create_data(filename):
@@ -48,26 +50,39 @@ async def async_create_data(filename):
     return filename
 
 
-# async def async_create_cors(ds):
-async def async_create_cors():
+async def async_create_cors(ds):
     cors = []
     i = 0
-    # for d in ds:
-    for i in range(DATA_SIZE):
+    # データセットのサイズ分タスクを作る
+    for d in ds:
         filename = "{}/{}.lz4".format(DIR, str(i).zfill(16))
-        cors.append(async_create_data(filename))
-        # cors.append(async_zip(filename,d))
-        # i += 2
+        # cors.append(async_create_data(filename))
+        cors.append(async_zip(filename, d))
+        i += 1
+    # タスクを実行
     done, pending = await asyncio.wait(cors)
     return done, pending
 
 
-# dataset = [create_items(ITEM_NUM) for x in range(DATA_SIZE)]
+# セマフォ　いったんグローバルにする
+sem = asyncio.Semaphore(2000)
 
-print("create dataset end: {}".format(datetime.datetime.now()))
+# データセットを作る
+dataset = [create_items(ITEM_NUM) for x in range(DATA_SIZE)]
+
+# ノンブロッキング用のループを作る
 loop = asyncio.get_event_loop()
-# done, pending = loop.run_until_complete(async_create_cors(dataset))
-done, pending = loop.run_until_complete(async_create_cors())
+
+start_time = datetime.datetime.now()
+
+# ループで実行するコルーチンを指定
+done, pending = loop.run_until_complete(async_create_cors(dataset))
+# done, pending = loop.run_until_complete(async_create_cors())
+
+finish_time = datetime.datetime.now()
+
+print("start  : {}".format(start_time))
+print("finish : {}".format(finish_time))
 
 for d in done:
     dr = d.result()
